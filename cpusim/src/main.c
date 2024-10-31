57b02b521b326a439337550845e0cf13d5dd74a4
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-10-30 21:24:50 -0400

    wip

--- cpusim/src/main.c
+++ cpusim/src/main.c
@@ -1,11 +1,43 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <err.h>
 
+struct program {
+    uint32_t *memory;
+    size_t memory_size;
+    size_t n_instructions;
+};
+
+struct program * input_hndl(char *fname){
+    FILE *pFile = fopen(fname, "rb");
+    if(!pFile) return NULL;
+    struct program * pProg = malloc(sizeof(struct program));
+    if(!pProg){
+        fclose(pFile);
+        return NULL;
+    }
+    fseek(pFile, 0, SEEK_END);
+    pProg->memory_size = ftell(pFile);
+    fseek(pFile, 0, SEEK_SET);
+    if((pProg->memory_size % 4) != 0){
+        fprintf(stderr, "program size not divisible by 4\n");
+        free(pProg);
+        fclose(pFile);
+        return NULL;
+    }
+    pProg->memory = (uint32_t*)malloc(pProg->memory_size);
+
+    return pProg;
+}
 
 int main(int argc, char **argv){
-    printf("Hello World\n");
-    for(int i = 0; i < argc; i++){
-        printf("Arg %d: %s\n", i, argv[i]);
+    if(argc < 2){
+        fprintf(stderr, "Incorrect Usage:\nInput file must be specified\n");
+        exit(1);
     }
+
     return 0;
 }
