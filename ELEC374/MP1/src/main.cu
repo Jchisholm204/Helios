f9e3fef79ebfa58af3611e606cd3baae4abf9c5c
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-02-17 13:46:03 -0500

    cpu mat mul

--- ELEC374/MP1/src/main.cu
+++ ELEC374/MP1/src/main.cu
@@ -85,25 +85,32 @@ void cudaAdd(void){
     cudaFree(C);
 }
 
-float *M_host, *N_host, *P_host, *P_ref;
-float *M_dev, *N_dev, *P_dev;
+typedef float* mat_t;
+mat_t M_host, N_host, P_host, P_ref;
+mat_t M_dev, N_dev, P_dev;
 #define MAT_N (3)
 #define MAT_SIZE (sizeof(float)*MAT_N*MAT_N)
 #define MAT(row, col) (MAT_N*row + col)
 
 #define FREE(p) if(!p){ free(p); p = NULL; }
 
-void MAT_print(float *Mat, size_t n){
+bool inRange(float val, float trg, float rng){
+    if (val > (trg+rng)) return false;
+    if (val < (trg-rng)) return false;
+    return true;
+}
+
+void MAT_print(mat_t Mat, size_t n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            printf("%5.1f ", Mat[MAT(i, j)]);
+            printf("%5.2f ", Mat[MAT(i, j)]);
         }
         printf("\n");
     }
 }
 
-void MAT_fillRand(float *Mat, int max_i, float div){
-    srand(time(NULL));
+void MAT_fillRand(mat_t Mat, int max_i, float div){
+    // srand(time(NULL));
     for(int i = 0; i < MAT_N; i++){
         for(int j = 0; j < MAT_N; j++){
             Mat[MAT(i, j)] = (float)(rand() % max_i)/div;
@@ -111,7 +118,7 @@ void MAT_fillRand(float *Mat, int max_i, float div){
     }
 }
 
-void MAT_fill(float *Mat, float val){
+void MAT_fill(mat_t Mat, float val){
     for(int i = 0; i < MAT_N; i++){
         for(int j = 0; j < MAT_N; j++){
             Mat[MAT(i, j)] = val;
@@ -119,12 +126,13 @@ void MAT_fill(float *Mat, float val){
     }
 }
 
-void cpuMatMul(float *P, float *M, float *N){
+void cpuMatMul(mat_t P, mat_t M, mat_t N){
     for(int i = 0; i < MAT_N; i++){
         for(int j = 0; j < MAT_N; j++){
-            P[MAT(i, j)] = 0;
+            double sum = 0;
             for(int k = 0; k < MAT_N; k++)
-                P[MAT(i, j)] += M[MAT(i, k)]*N[MAT(k, j)];
+                sum += M[MAT(i, k)]*N[MAT(k, j)];
+            P[MAT(i, j)] = sum;
         }
     }
 }
@@ -136,14 +144,14 @@ int main() {
     // cudaAdd();
 
     // Allocate Host side memories
-    M_host = (float*)malloc(MAT_SIZE);
-    N_host = (float*)malloc(MAT_SIZE);
-    P_host = (float*)malloc(MAT_SIZE);
-    P_ref  = (float*)malloc(MAT_SIZE);
+    M_host = (mat_t)malloc(MAT_SIZE);
+    N_host = (mat_t)malloc(MAT_SIZE);
+    P_host = (mat_t)malloc(MAT_SIZE);
+    P_ref  = (mat_t)malloc(MAT_SIZE);
 
     // Fill the matricies with data
-    MAT_fill(M_host, 1);
-    MAT_fill(N_host, 2);
+    MAT_fillRand(M_host, 10, 1.4);
+    MAT_fillRand(N_host, 11, 1.8);
     MAT_fill(P_host, 0);
 
     // Allocate Device Side Memory
@@ -154,12 +162,18 @@ int main() {
     // Copy the data from the host to the device
     cudaMemcpy(&M_dev, M_host, MAT_SIZE, cudaMemcpyHostToDevice);
     cudaMemcpy(&N_dev, N_host, MAT_SIZE, cudaMemcpyHostToDevice);
-    
+
+    // Run the Multiplication Kernel
+
     // Copy the result back to the host
-    cudaMemcpy(&P_host, P_dev, MAT_SIZE, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(&P_host, P_dev, MAT_SIZE, cudaMemcpyDeviceToHost);
 
     // Compute the reference matrix
     cpuMatMul(P_ref, N_host, M_host);
+    printf("Matrix A:\n");
+    MAT_print(N_host, MAT_N);
+    printf("Matrix B:\n");
+    MAT_print(M_host, MAT_N);
     printf("Matrix CPU Multiplication Result:\n");
     MAT_print(P_ref, MAT_N);
 
