b6bbe59cebae4e138183caa5cb306a3b210f7970
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-01-05 15:28:37 -0500

    wip

--- notebook_index/main.py
+++ notebook_index/main.py
@@ -3,21 +3,40 @@ import os
 VAULT_SOURCE_DIR = "sample_vault"
 
 
-def print_dir(base_dir: str, indent: int = 1):
+class Directory:
+    path: str
+    subdirs: list
+    files: list
+
+    def __init__(self, name: str):
+        self.path = name
+        self.subdirs = []
+        self.files = []
+
+    def print(self, indent: int = 0):
+        print("  "*indent + os.path.basename(self.path))
+        print("    "*indent + f"->{self.files}")
+        for subdir in self.subdirs:
+            subdir.print(indent=indent+1)
+
+
+def scan_children(base_dir: str) -> Directory:
     if not os.path.exists(base_dir) or not os.path.isdir(base_dir):
-        return
+        return None
+    found: Directory = Directory(base_dir)
     dirs = os.listdir(base_dir)
-    # print(os.path.basename(base_dir))
     for dir in dirs:
-        print("|"*indent + f"> {dir}")
-        if os.path.isdir(os.path.join(base_dir, dir)):
-            print_dir(os.path.join(base_dir, dir), indent+1)
+        sdir = os.path.join(base_dir, dir)
+        if os.path.isdir(sdir):
+            found.subdirs.append(scan_children(sdir))
+        else:
+            found.files.append(dir)
+    return found
 
 
 def main():
     wd = os.path.join(os.getcwd(), VAULT_SOURCE_DIR)
-    print(f"> {os.path.basename(wd)}")
-    print_dir(wd)
+    scan_children(wd).print()
 
 
 if __name__ == "__main__":
